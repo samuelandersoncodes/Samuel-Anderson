ec4effd5bb84e2aec92f47f1918f3789749a040d
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-09-15 14:53:04 +0000

    feat: Add code to send email

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
 document.getElementById('copyright-date').innerHTML = (new Date().getFullYear());
+
+/*
+ * this function enables users
+ * through the contact form 
+ * to send offer messages via EmailJS
+ */
+function sendMail() {
+
+    var params = {
+        from_name: document.getElementById('fullname').value,
+        email_id: document.getElementById('email').value,
+        message: document.getElementById('offertype').value,
+    };
+
+    emailjs.send('service_6bnphex', 'template_mzd0432', params)
+        .then((res) => {
+            alert(`Message sent successfully ${res.status}`);
+        });
+};
