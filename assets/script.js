dea7b73e4369622a1f6acd09603858f9b8299994
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-04-06 20:02:16 +0200

    feat: Highlight navbar active item

--- assets/script.js
+++ assets/script.js
@@ -27,3 +27,33 @@ document.querySelectorAll('.navigation .nav-link').forEach(item => {
         offcanvas.hide();
     });
 });
+
+// Highlights nav active items
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section[id]");
+    const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+    window.addEventListener("scroll", () => {
+        let current = "";
+
+        // Explicitly check if scroll position is at the top of the page
+        if (pageYOffset === 0) {
+            current = "header";
+        } else {
+            sections.forEach((section) => {
+                const sectionTop = section.offsetTop;
+                const sectionHeight = section.clientHeight;
+                if (pageYOffset >= sectionTop - sectionHeight / 3) {
+                    current = section.getAttribute("id");
+                }
+            });
+        }
+
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").slice(1) === current) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
